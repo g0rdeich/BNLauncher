33ac632f6df043737a701ea280e3f9c790f1081e
Alex Gordeev <gordeeffff@gmail.com>
2020-10-06 11:52:58 +0300

    init: push bnlauncher to git

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BNLauncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        async void Form1_Load(object sender, EventArgs e)
+        {
+            Cmd("start \"\" \"D:\\Battle.net\\Battle.net Launcher.exe\"");
+            await Task.Delay(15000);
+            Cmd("start \"\" C:\\Users\\Alex\\AppData\\Local\\HearthstoneDeckTracker\\HearthstoneDeckTracker.exe");
+            await Task.Delay(45000);
+            if (Process.GetProcessesByName("Hearthstone").Length > 0)
+            {
+                await Task.Delay(2000);
+                Cmd($"taskkill /f /pid \"{Process.GetCurrentProcess().Id}\" &" + 
+                    $"taskkill /f /im Battle.net.exe &" +
+                    $"taskkill /f /im Agent.exe");
+            }
+            
+            
+        }
+
+        void Cmd(string line)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                Arguments = $"/c {line}",
+                WindowStyle = ProcessWindowStyle.Hidden
+            }).WaitForExit();
+        }
+    }
+}
